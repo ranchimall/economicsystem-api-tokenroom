6b707bce191b4981dd81c6673cbc4ec3dab68d14
Vivek Teega <vivek.teega@gmail.com>
2023-06-01 18:05:07 +0530

    Updated formula for consumption valuation

--- fetch_finance_data.js
+++ fetch_finance_data.js
@@ -2,11 +2,12 @@ const { google } = require('googleapis');
 const privatekey = require('./access_token.json');
 const sheet_data = require('./sheet_data.json');
 
-class RanchiMallData {
+class EconomicSystem {
   constructor() {
     this.productionCost = 0;
-    this.consumptionCost = 0;
     this.productionValuation = 0;
+    this.consumptionNumber = 0;
+    this.consumptionCost = 0;
     this.consumptionValuation = 0;
     this.spreadsheetId = sheet_data.spreadsheetId;
   }
@@ -22,9 +23,8 @@ class RanchiMallData {
     const sheets = google.sheets({ version: 'v4', auth });
 
     try {
-
       const response = await sheets.spreadsheets.values.get({
-        spreadsheetId: this.spreadsheetId,
+        spreadsheetId: sheet_data.revenue_spreadsheetId,
         range: sheet_data.TR_Expenses_range, // Assuming consumption data is in column B, starting from row 7
         valueRenderOption: 'FORMULA' // Fetch the formula values instead of the displayed values
       });
@@ -45,6 +45,39 @@ class RanchiMallData {
     }
   }
 
+  async fetchConsumptionNumber(){
+    const auth = new google.auth.JWT(
+      privatekey.client_email,
+      null,
+      privatekey.private_key,
+      ['https://www.googleapis.com/auth/spreadsheets']
+    );
+
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    try {
+      const response = await sheets.spreadsheets.values.get({
+        spreadsheetId: sheet_data.booking_spreadsheetId,
+        range: sheet_data.Booking_range, // Assuming consumption data is in column B, starting from row 7
+        valueRenderOption: 'UNFORMATTED_VALUE' // Fetch the formula values instead of the displayed values
+      });
+      
+      const consumptionData = response.data.values;
+      if (consumptionData) {
+        let sum = 0;
+        for (const row of consumptionData) {
+          const value = parseFloat(row[0]);
+          if (!isNaN(value)) {
+            sum += value;
+          }
+        }
+        this.consumptionNumber = sum
+      }
+    } catch (error) {
+      console.error('Error fetching consumption data:', error);
+    }
+  }
+
   async fetchConsumptionData() {
     const auth = new google.auth.JWT(
       privatekey.client_email,
@@ -56,10 +89,9 @@ class RanchiMallData {
     const sheets = google.sheets({ version: 'v4', auth });
 
     try {
-
       const response = await sheets.spreadsheets.values.get({
-        spreadsheetId: this.spreadsheetId,
-        range: sheet_data.TR_Expenses_range, // Assuming consumption data is in column B, starting from row 7
+        spreadsheetId: sheet_data.revenue_spreadsheetId,
+        range: sheet_data.TR_Consumption_range, // Assuming consumption data is in column B, starting from row 7
         valueRenderOption: 'FORMULA' // Fetch the formula values instead of the displayed values
       });
       
@@ -80,15 +112,15 @@ class RanchiMallData {
   }
 
   calculateConsumptionValuation() {
-    this.consumptionValuation = this.consumptionCost * 0.8;
+    this.consumptionValuation = this.consumptionNumber * sheet_data.TR_Consumption_Valuation_Price_USD;
     return this.consumptionValuation;
   }
 
   calculateProductionValuation() {
-    this.productionValuation = this.productionCost * 1.2;
+    this.productionValuation = this.productionCost;
     return this.productionValuation;
   }
-
+ 
   calculateSystemValuation() {
     return Math.max(this.productionValuation, this.consumptionValuation);
   }
@@ -96,16 +128,18 @@ class RanchiMallData {
 
 // Example usage
 async function fetchDataFromGoogleSheet() {
-  const ranchiMallData = new RanchiMallData();
+  const tokenRoom_EconomicSystem = new EconomicSystem();
   
-  await ranchiMallData.fetchProductionData();
-  await ranchiMallData.fetchConsumptionData();
+  await tokenRoom_EconomicSystem.fetchProductionData();
+  await tokenRoom_EconomicSystem.fetchConsumptionData();
+  await tokenRoom_EconomicSystem.fetchConsumptionNumber();
 
-  console.log('Total production cost for RanchiMall:', ranchiMallData.productionCost);
-  console.log('Total consumption cost by users:', ranchiMallData.consumptionCost);
-  console.log('Total pxpense valuation of RanchiMall:', ranchiMallData.calculateProductionValuation());
-  console.log('Total consumption valuation of users:', ranchiMallData.calculateConsumptionValuation());
-  console.log('System valuation:', ranchiMallData.calculateSystemValuation());
+  console.log('Total production cost for RanchiMall:', tokenRoom_EconomicSystem.productionCost);
+  console.log('Total consumption:', tokenRoom_EconomicSystem.consumptionNumber);
+  console.log('Total amount in consumption:', tokenRoom_EconomicSystem.consumptionCost);
+  console.log('Total production valuation:', tokenRoom_EconomicSystem.calculateProductionValuation());
+  console.log('Total consumption valuation:', tokenRoom_EconomicSystem.calculateConsumptionValuation());
+  console.log('System valuation:', tokenRoom_EconomicSystem.calculateSystemValuation());
 }
 
 fetchDataFromGoogleSheet();
