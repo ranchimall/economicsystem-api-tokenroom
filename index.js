cc3c3352cd03c902b2d44a37c957ef906715b848
RanchiMall Dev <ranchimallfze@gmail.com>
2023-06-02 16:35:35 +0000

    cors

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const app = express();
 const economicSystemRoutes = require('./routes/economicSystem');
 
 // Middleware
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 // Routes
 app.use('/api/economicSystem', economicSystemRoutes);
