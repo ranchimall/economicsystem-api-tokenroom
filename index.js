cbfb86a7e7f6e6dcf237d51e7b9e752d77eed5c3
RanchiMall Dev <ranchimallfze@gmail.com>
2023-06-02 16:48:17 +0000

    Fixed cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const economicSystemRoutes = require('./routes/economicSystem');
 
 // Middleware
+app.use(express.json())
 app.use(cors());
 
 // Routes
