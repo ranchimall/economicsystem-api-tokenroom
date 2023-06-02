3e383d19b16160b34764303808544c00981d5d72
Vivek Teega <vivek.teega@gmail.com>
2023-06-02 21:57:42 +0530

    Cors policy

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const economicSystemRoutes = require('./routes/economicSystem');
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Routes
 app.use('/api/economicSystem', economicSystemRoutes);
